af769d30b00886db9a3718972088772824bd8f5c
Scoops Martin <scoops@Scoopss-Mini.lan>
2024-05-22 14:46:06 -0500

    Created test and solved problem 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -9,7 +9,10 @@
 
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 45 (euler-1 13))
+    (should= 60 (euler-1 16))
+    (should= -1 (euler-1 1000))
+    )
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,12 @@
 (ns euler.level1.problem001)
 
 (defn euler-1 [n]
+  (loop [num (dec n)
+         vec []]
+    (if (= num 0)
+      (reduce + vec)
+      (recur (dec num) (if (or (= (mod num 3) 0)
+                             (= (mod num 5) 0))
+                       (conj vec num)
+                       vec))))
   )
